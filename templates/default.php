77e15ccf0273f2ec9252d39e975884088aefd97f
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-01-23 23:21:42 +0100

    Update templates/default.php

--- templates/default.php
+++ templates/default.php
@@ -4,6 +4,7 @@
 </head>
 
 <body>
+<a href="https://github.com/mevdschee/Cravetivity.com"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
 <h1>Cravetivity</h1>
 <?php echo $body; ?>
 <br/><br/>
